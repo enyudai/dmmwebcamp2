4e89a4b8dd1988e94224222b69d0ceb64b114e2f
EC2 Default User <ec2-user@ip-172-31-27-158.ap-northeast-1.compute.internal>
2022-11-09 09:53:13 +0000

    Commit1

--- ruby/new.rb
+++ ruby/new.rb
@@ -1,15 +1,28 @@
+# インスタンスメソッド
 class Car
-    def turn(direction)
+  
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.move("右", 5)
+
+# クラスメソッド
+class Car
+    def self.turn(direction)
         puts "#{direction}に曲がります"
-    end
-    
-    def run(distance)
-        puts "車では#{distance}キロ走ります。"
     end
 end
 
-car=Car.new
-car.turn("右")
-
-car=Car.new
-car.run(5)
+Car.turn("右")
